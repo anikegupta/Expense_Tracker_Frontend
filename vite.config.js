d49d31729277f5a8f6d6003df3b5cc196f50c9d4
Aniket <aniketgupta3625@gmail.com>
2025-11-07 16:37:49 +0530

    added

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import flowbiteReact from "flowbite-react/plugin/vite"
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'http://localhost:8080',
+      '/api':'http://localhost:8010',
     },
   },
   plugins: [react(),tailwindcss(),flowbiteReact()],
